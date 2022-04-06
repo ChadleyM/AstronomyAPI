fc9426ec78286e62fc027a2fcad4539587246836
Chadley Maggott <chadleym@bbd.co.za>
2022-04-06 07:33:19 +0200

    Initial spring set up

---
+++ src/main/java/com/group8/stargaming/StargamingApplication.java
@@ -0,0 +1,13 @@
+package com.group8.stargaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group8/stargaming/StargamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group8.stargaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
